7fd7b1b6b6a207809014796c914c1ef94dd944da
PRAJWAL KARALE <prajwalkarale@PRAJWALs-MacBook-Pro.local>
2023-06-24 22:39:16 +0530

    Added CSS styling using bulma

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import ProfileCard from './ProfileCard'
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+import Typewriter from 'typewriter-effect';
+
+// console.log(AlexaImage);
+// console.log(SiriImage);
+// console.log(CortanaImage);
 
 function App() {
     return (
         <div>
-            <div>
-                Personal Digital Assistant
+            <section class="hero is-primary">
+            <div class="hero-body">
+                <p class="title">
+                        <Typewriter
+                        onInit={(typewriter) => {
+                        typewriter
+                            .typeString("PERSONAL DIGITAL ASSISTANTS")
+                            .pauseFor(1000)
+                            .deleteAll()
+                            .typeString("WELCOMES YOU!!")
+                            .start();
+                    }}
+                    />
+                </p>
             </div>
-            <ProfileCard title = "Alexa" handle = "@alexa99" />
-            <ProfileCard title = "Cortana" handle = "@cortana99" />
-            <ProfileCard title = "Siri" handle = "@Siri99" />
+            </section>
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard title = "Alexa" handle = "@alexa99" image = {AlexaImage} />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title = "Cortana" handle = "@cortana99"  image = { CortanaImage } />
+
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title = "Siri" handle = "@Siri99" image = { SiriImage } />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,20 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({title, handle, image}) {
     //Destructuring the props {title, handle} = props
     return (
         <div>
-            <div>The title is {title}</div>
-            <div>The handle is {handle}</div>
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-1by1">
+                        <img src={image} alt="personal-digital-assistant logo" />
+                    </figure>
+                </div>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">The title is {title}</p>
+                    <p className="subtitle is-6">The handle is {handle}</p>
+                </div>
+            </div>
         </div>
     );
 }
